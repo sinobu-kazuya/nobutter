c72723ac0946292d09b1c0e72b8d4776ef6e27be
信夫知也 <kazooie.nobuo@gmail.com>
2018-08-26 21:22:36 +0900

    Create nickname column, add nickname post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
   def show
+    @nickname = cuurent_user.nickname
+    @tweets = Tweet.where(user_id: current_user.id).order("created_at DESC")
   end
 end
