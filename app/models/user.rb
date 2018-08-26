ff57e5b1ee6774a8b24813185c64b0a087b7761f
信夫知也 <kazooie.nobuo@gmail.com>
2018-08-26 21:24:35 +0900

    Add association likes table

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :tweets
+  has_many :likes, dependent: :destroy
+  has_many :like_tweets, through: :likes, source: :tweet
 end
