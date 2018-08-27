a6329362309122a486bb8e8315d1c4b5e2aebe61
信夫知也 <kazooie.nobuo@gmail.com>
2018-08-27 20:56:29 +0900

    Delete indent & a little change

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,29 +3,37 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.includes(:user).order("created_at DESC")
   end
+
   def new
     @tweet = Tweet.new
   end
+
   def create
     @tweet = Tweet.new(text: tweet_params[:text], image: tweet_params[:image], name: tweet_params[:name], user_id: current_user.id)
     @tweet.save
   end
+
   def destroy
     tweet = Tweet.find(params[:id])
     tweet.destroy if tweet.user.id = current_user.id
   end
+
   def edit
   end
+
   def update
   end
+
   def show
     @tweet = Tweet.find(params[:id])
+    @comment = Comment.new
+    @comments = @tweet.comments.includes(:user)
   end
 
   private
 
   def tweet_params
-    params.require(:tweet).permit(:text, :image, :name).merge(user_id: current_user.id)
+    params.require(:comment).permit(:text, :image, :name).merge(user_id: current_user.id)
   end
 
 end
