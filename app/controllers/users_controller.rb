85dc8be417588610525fd905e58479b777426096
信夫知也 <kazooie.nobuo@gmail.com>
2018-08-31 15:45:41 +0900

    Add avatar to strong parameter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name,:email, :image)
+    params.require(:user).permit(:name, :email, :image, :avatar)
   end
 end
