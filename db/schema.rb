3818e78934f98f159f2c136fe79a90e6b1f68410
信夫知也 <kazooie.nobuo@gmail.com>
2018-08-31 15:44:01 +0900

    Add column to users avatar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180830114613) do
+ActiveRecord::Schema.define(version: 20180831025508) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "user_id"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20180830114613) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.string "name"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
