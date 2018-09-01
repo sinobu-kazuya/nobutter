2eea8e4768243e740a72c3c18fd41cd8e4e71c44
信夫知也 <kazooie.nobuo@gmail.com>
2018-09-01 12:30:05 +0900

    Create relationship table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180831025508) do
+ActiveRecord::Schema.define(version: 20180831081640) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "user_id"
